e7496a672c1e353850f3fb72d2ed8ca17aa670c8
Kalina Petersen <petersenkalina@gmail.com>
2023-02-07 23:12:26 -0800

    Create how calculator looks

---
+++ script.js
@@ -0,0 +1,17 @@
+const person = {
+    name: {
+        first: "Bob",
+        last: "Smith",
+      },
+    age: 32,
+    bio: function () {
+      console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+    },
+    introduceSelf: function () {
+      console.log(`Hi! I'm ${this.name[0]}.`);
+    },
+  };
+
+const myDataName = "height";
+const myDataValue = "1.75m";
+person[myDataName] = myDataValue;
