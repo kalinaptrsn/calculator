7bb621df2eb12a48f23af3e0536b07365bc8883f
Kalina Petersen <petersenkalina@gmail.com>
2023-02-15 22:38:44 -0800

    Add operation and compute functions

--- script.js
+++ script.js
@@ -11,26 +11,63 @@ class Calculator {
         this.operation = undefined;
     };
 
-    delete(){
+    delete() {
 
     };
 
     appendNumber(number) {
-        this.currentOperand = number;
+        if(number === '.' && this.currentOperand.includes('.'))
+            return; 
+        this.currentOperand = this.currentOperand.toString() + number.toString();
     };
 
     chooseOperation(operation) {
-
+        if (this.currentOperand === '') return;
+        if (this.previousOperand != '') {
+            this.compute();
+        } else {
+            this.operation = operation;
+            this.previousOperand = this.currentOperand;
+            this.currentOperand = '';
+        }
     };
 
     compute() {
-
+        let computation;
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) return;
+        switch (this.operation) {
+            case '+':
+                computation = prev + current
+                break
+            case '-':
+                computation = prev - current
+                break
+            case 'x':
+                computation = prev * current
+                break
+            case '÷':
+                computation = prev / current
+                break
+            default:
+                return
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
     };
 
     updateDisplay() {
         this.currentOperandTextElement.innerText = this.currentOperand;
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText = 
+            this.previousOperand + ' ' + this.operation
+        } else {
+            this.previousOperandTextElement.innerText = '';
+            }
+        }
     };
-}
 
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
@@ -48,6 +85,29 @@ numberButtons.forEach(button => {
         calculator.updateDisplay();
     });
 });
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    });
+});
+
+equalsButton.addEventListener('click', button => {
+        calculator.compute();
+        calculator.updateDisplay();
+    });
+
+deleteButton.addEventListener('click', button => {
+        calculator.delete();
+        calculator.updateDisplay();
+    });
+
+clearButton.addEventListener('click', button => {
+        calculator.clear();
+        calculator.updateDisplay();
+    });
+
 
 function add(a,b) {
     return a + b;
